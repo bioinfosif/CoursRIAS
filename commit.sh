ad574229223261242dcdcc25ce94d7078bfe9cca
bioinfosif <ndaom403@gmail.com>
2022-02-09 00:00:01 +0000

    commit_msg

--- commit.sh
+++ commit.sh
@@ -6,4 +6,4 @@ read commit_msgs
 git add .
 git commit -m commit_msg
 git push https://github.com/bioinfosif/CoursRIAS.git
-#!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
+#!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
